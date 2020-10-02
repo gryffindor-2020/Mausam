2c17e5dd81b7f9c98eed82e5ea2b21dcf5c8f1dc
himankgoel <himankgoel12@gmail.com>
2020-10-03 00:04:48 +0530

    Completed first phase css

--- src/Components/UserProf.js
+++ src/Components/UserProf.js
@@ -2,7 +2,18 @@ import React from "react";
 
 function Prof(){
     return <div className="user-profile">
-            Profile
+            <div className="location">
+                <h2 className="location__city">The Hague,</h2>
+                <h3 className="location__country">Netherlands</h3>
+            </div>
+            <div className="date">Dec 31,2019</div>
+            <h1 className="today">Today</h1>
+            <div className="temperature">
+                <img src={require("../sun.png")} className="temperature__img" alt="Current Weather icon"/>
+                <h1 className="temperature__value">19</h1>
+                <span className="temperature__unit">&#176; C</span> 
+            </div>
+
         </div>
 }
 export default Prof;
