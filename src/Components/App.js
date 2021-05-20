fb5c9126c7e78760fa3b72081f75097022c29429
Salar Khan <salarkhan586@gmail.com>
2021-05-20 13:55:56 +0530

    Made App.js as functional component from class component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useEffect} from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { connect } from "react-redux";
 import Nav from "./Nav";
@@ -7,24 +7,19 @@ import Dashboard from "./Dashboard";
 import { setCurrentCity } from "../redux/city/city.actions";
 
 const API_KEY = "5f38a955153345aebbc145451200410";
-const API_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=delhi&aqi=yes`;
+const API_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=mumbai&aqi=yes`;
 
-async function apicall() {
-  const data = await fetch(API_URL)
-    .then((res) => res.json())
-    .then((data) => {
-      return data;
-    });
-  return data;
-}
-
-class App extends React.Component {
-  async componentDidMount() {
-    this.props.setCurrentCity({
-      city: await apicall(),
-    });
-  }
-  render() {
+function App (props) {
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json()) 
+      .then((data) => {
+        props.setCurrentCity({
+          ...data
+        });
+      });
+  });
+ 
     return (
       <Router>
         <div className="container">
@@ -33,7 +28,6 @@ class App extends React.Component {
         </div>
       </Router>
     );
-  }
 }
 const mapDispatchToProps = (dispatch) => ({
   setCurrentCity: (city) => dispatch(setCurrentCity(city)),
