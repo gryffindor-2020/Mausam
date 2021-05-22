95bca916e328260d6f8ac5eb3574433d90ca0e03
himankgoel <himankgoel12@gmail.com>
2021-05-22 17:19:17 +0530

    Dynamicly loaded data now

--- src/Components/Highlights.js
+++ src/Components/Highlights.js
@@ -3,15 +3,26 @@ import { connect } from "react-redux";
 
 import Tile from "./HighlightTile";
 
-function Highlights({ currentCity: { location, current }, city }) {
-  console.log(location);
+function Highlights({ currentCity: { current } }) {
   return (
     <div className="highlights">
       <h2 className="highlights__heading">Today's Highlights</h2>
       <div className="highlights__info">
-        {city.map((el, index) => {
-          return <Tile object={el} key={index} />;
-        })}
+        <Tile object={{ type: "UV Index", value: current.uv }} />
+        <Tile
+          object={{
+            type: "Wind Status",
+            value: current.wind_kph,
+            unit: "km/hr",
+            comment: current.wind_dir,
+          }}
+        />
+        <Tile
+          object={{ type: "Visibility", value: current.vis_km, unit: "km" }}
+        />
+        <Tile
+          object={{ type: "Humidity", value: current.humidity, unit: "%" }}
+        />
       </div>
     </div>
   );
