dc0c87513328b3525e289356d1220d07f273f1f7
himankgoel <himankgoel12@gmail.com>
2021-05-22 12:29:54 +0530

    Removed API calls

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,35 +1,16 @@
-import React,{useEffect} from "react";
+import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import { connect } from "react-redux";
 import Nav from "./Nav";
 import Dashboard from "./Dashboard";
 
-import { setCurrentCity } from "../redux/city/city.actions";
-
-const API_KEY = "5f38a955153345aebbc145451200410";
-const API_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=mumbai&aqi=yes`;
-
-function App (props) {
-  useEffect(() => {
-    fetch(API_URL)
-      .then((res) => res.json()) 
-      .then((data) => {
-        props.setCurrentCity({
-          ...data
-        });
-      });
-  });
- 
-    return (
-      <Router>
-        <div className="container">
-          <Nav />
-          <Dashboard />
-        </div>
-      </Router>
-    );
+function App(props) {
+  return (
+    <Router>
+      <div className="container">
+        <Nav />
+        <Dashboard />
+      </div>
+    </Router>
+  );
 }
-const mapDispatchToProps = (dispatch) => ({
-  setCurrentCity: (city) => dispatch(setCurrentCity(city)),
-});
-export default connect(null, mapDispatchToProps)(App);
+export default App;
