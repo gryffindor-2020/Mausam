b8ee48b6ea104e660f76bc665dbd1b03bb7d0c26
himankgoel <himankgoel12@gmail.com>
2021-05-22 12:30:24 +0530

    Added API call in dashboard and loading with spinner

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
+import { connect } from "react-redux";
 import Highlights from "./Highlights";
 import Profile from "./UserProf";
+import withSpinner from "./spinner/spinner";
 
-function Dashboard() {
+import { setCurrentCity } from "../redux/city/city.actions";
+const API_KEY = "5f38a955153345aebbc145451200410";
+const API_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=mumbai&aqi=yes`;
+
+const HighlightsWithSpinner = withSpinner(Highlights);
+const ProfileWithSpinner = withSpinner(Profile);
+function Dashboard(props) {
+  const [isLoading, setIsLoading] = useState(true)
   let data1 = [];
   const uv = { type: "UV Index", value: 8 };
   const windStatus = {
@@ -25,12 +34,24 @@ function Dashboard() {
   data1.push(windStatus);
   data1.push(visibility);
   data1.push(humidity);
-
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json()) 
+      .then((data) => {
+        props.setCurrentCity({
+          ...data
+        });
+        setIsLoading(false);
+      });
+  });
   return (
     <React.Fragment>
-      <Highlights city={data1} />
-      <Profile />
+      <HighlightsWithSpinner isLoading={isLoading} city={data1} />
+      <ProfileWithSpinner isLoading={isLoading} />
     </React.Fragment>
   );
 }
-export default Dashboard;
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentCity: (city) => dispatch(setCurrentCity(city)),
+});
+export default connect(null, mapDispatchToProps)(Dashboard);
