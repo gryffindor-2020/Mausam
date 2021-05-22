6483a3e288f6e5219019d2efcd8254ace1eb7995
himankgoel <himankgoel12@gmail.com>
2021-05-22 17:20:13 +0530

    d

--- src/Components/AddLocation.js
+++ src/Components/AddLocation.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function AddLocation() {
   // const API_KEY = "5f38a955153345aebbc145451200410";
   // const API_URL = `http://api.weatherapi.com/v1/search.json?key=${API_KEY}&q=${query}`;
 
+  const [query, setQuery] = useState("");
+  // const [result, setResults] = useState();
+  async function handleChange(e) {
+    setQuery(e.target.value);
+    // const searchResults = await retrieveMovies(e.target.value);
+    // setResults(searchResults);
+  }
+  function handleClick() {
+    setQuery("");
+  }
   return (
     <div className="add-location">
-      {/* <div className="add-location__search">
-        <InputEvent
+      <div className="add-location__search">
+        <input
+          value={query}
           placeholder="Delhi, New York ..."
           type="text"
           className="add-location__input"
@@ -21,10 +32,10 @@ function AddLocation() {
             onClick={handleClick}
           />
         )}
-        {query !== "" && results.length !== 0 && (
+        {/* {query !== "" && results.length !== 0 && (
           <SearchResults results={results} handleSelectCity={handleClick} />
-        )}
-      </div> */}
+        )} */}
+      </div>
     </div>
   );
 }
