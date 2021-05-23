74a8f233acdeb2689ff3ce13db0e95faf2a3d2bf
himankgoel <himankgoel12@gmail.com>
2021-05-23 08:56:18 +0530

    Added loading of data in search bar

--- src/Components/SearchResultIndividual.js
+++ src/Components/SearchResultIndividual.js
@@ -1,16 +1,14 @@
 import React from "react";
 
-function SearchResult(props) {
-  // const API_KEY = "5f38a955153345aebbc145451200410";
-  // const API_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`;
-
+// const API_KEY = "5f38a955153345aebbc145451200410";
+// const API_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`;
+function SearchResult({ data: { name, region, country, lat, lon } }) {
   function handleClick() {
-    // console.log(city);
+    console.log(lat, lon);
   }
-
   return (
     <div className="search-result__individual" onClick={handleClick}>
-      <span className="search-result__text">Wakanda</span>
+      <span className="search-result__text">{`${name},${region},${country}`}</span>
       <i className="fas fa-plus-square search-result__icon" />
     </div>
   );
