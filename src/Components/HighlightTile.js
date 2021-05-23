eb12c6a84e902eaa0c80d332266363daed1615f4
himankgoel <himankgoel12@gmail.com>
2021-05-23 15:27:30 +0530

    Added math.round in highlight to fix overflow

--- src/Components/HighlightTile.js
+++ src/Components/HighlightTile.js
@@ -5,7 +5,9 @@ function Tile(props) {
     <div className="highlights__tile">
       <h3 className="highlights__tile--heading">{props.object.type}</h3>
       <div className="highlights__tile--data">
-        <h1 className="highlights__tile--value">{props.object.value}</h1>
+        <h1 className="highlights__tile--value">
+          {Math.round(props.object.value)}
+        </h1>
         <h3 className="highlights__tile--unit">{props.object.unit}</h3>
       </div>
       <h4 className="highlights__tile--comment">
