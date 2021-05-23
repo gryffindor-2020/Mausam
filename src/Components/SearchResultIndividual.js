cc67c4e8632aca981ea6f5ad9c8d381e17367561
himankgoel <himankgoel12@gmail.com>
2021-05-23 19:07:40 +0530

    Added aqi=yes in API_URL

--- src/Components/SearchResultIndividual.js
+++ src/Components/SearchResultIndividual.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import {connect} from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 import { setCurrentCity } from "../redux/city/city.actions";
 const API_KEY = "5f38a955153345aebbc145451200410";
-// const API_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`;
-function SearchResult({ setCurrentCity ,data: { name, region, country, lat, lon } }) {
+function SearchResult({
+  setCurrentCity,
+  data: { name, region, country, lat, lon },
+}) {
   function handleClick() {
-    const API_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${lat}${' '}${lon}`;
+    const API_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${lat}${" "}${lon}&aqi=yes`;
     fetch(API_URL)
       .then((res) => res.json())
       .then((data) => {
@@ -13,7 +15,7 @@ function SearchResult({ setCurrentCity ,data: { name, region, country, lat, lon 
           ...data,
         });
       });
-    }
+  }
   return (
     <div className="search-result__individual" onClick={handleClick}>
       <span className="search-result__text">{`${name},${region},${country}`}</span>
@@ -24,4 +26,4 @@ function SearchResult({ setCurrentCity ,data: { name, region, country, lat, lon 
 const mapDispatchToProps = (dispatch) => ({
   setCurrentCity: (city) => dispatch(setCurrentCity(city)),
 });
-export default connect(null,mapDispatchToProps)(SearchResult);
+export default connect(null, mapDispatchToProps)(SearchResult);
