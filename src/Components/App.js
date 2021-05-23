544cb1cc1f573d1bb888b1c7b23294c1e848e358
himankgoel <himankgoel12@gmail.com>
2021-05-23 16:49:03 +0530

    Added API call in App.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,16 +1,39 @@
-import React from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import { connect } from "react-redux";
 import Nav from "./Nav";
 import Dashboard from "./Dashboard";
+import withSpinner from "./spinner/spinner";
 
-function App() {
+import { setCurrentCity } from "../redux/city/city.actions";
+const API_KEY = "5f38a955153345aebbc145451200410";
+const API_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=mumbai&aqi=yes`;
+
+const DashboardWithSpinner = withSpinner(Dashboard);
+
+function App(props) {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        props.setCurrentCity({
+          ...data,
+        });
+        setIsLoading(false);
+      });
+  }, []);
   return (
     <Router>
       <div className="container">
         <Nav />
-        <Dashboard />
+        <DashboardWithSpinner isLoading={isLoading} />
       </div>
     </Router>
   );
 }
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentCity: (city) => dispatch(setCurrentCity(city)),
+});
+export default connect(null, mapDispatchToProps)(App);
