9dcac491cbc2afa843c69e1c526510b1377e1a32
himankgoel <himankgoel12@gmail.com>
2021-05-23 08:54:27 +0530

    Added search-bar

--- src/Components/AddLocation.js
+++ src/Components/AddLocation.js
@@ -1,19 +1,24 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import SearchResults from "./SearchResults";
 function AddLocation() {
-  // const API_KEY = "5f38a955153345aebbc145451200410";
-  // const API_URL = `http://api.weatherapi.com/v1/search.json?key=${API_KEY}&q=${query}`;
+  const API_KEY = "5f38a955153345aebbc145451200410";
 
   const [query, setQuery] = useState("");
-  // const [result, setResults] = useState();
-  async function handleChange(e) {
+  const [results, setResults] = useState([]);
+  function handleChange(e) {
     setQuery(e.target.value);
-    // const searchResults = await retrieveMovies(e.target.value);
-    // setResults(searchResults);
   }
   function handleClick() {
     setQuery("");
   }
+  useEffect(() => {
+    const API_URL = `http://api.weatherapi.com/v1/search.json?key=${API_KEY}&q=${query}`;
+    if (query.length >= 3) {
+      fetch(API_URL)
+        .then((res) => res.json())
+        .then((data) => setResults(data.slice(0, 4)));
+    }
+  }, [query]);
   return (
     <div className="add-location">
       <div className="add-location__search">
@@ -28,13 +33,13 @@ function AddLocation() {
         {query !== "" && (
           <i
             type="button"
-            className="fas fa-times add-location__icon"
+            className="fas fa-times add-location__icon add_location__icon--remove"
             onClick={handleClick}
           />
         )}
-        {/* {query !== "" && results.length !== 0 && (
-          <SearchResults results={results} handleSelectCity={handleClick} />
-        )} */}
+        {query !== "" && results.length !== 0 && (
+          <SearchResults results={results} />
+        )}
       </div>
     </div>
   );
