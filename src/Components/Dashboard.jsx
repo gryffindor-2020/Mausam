6decb861fad7a9c0489184cbb492d17861081933
himankgoel <himankgoel12@gmail.com>
2021-05-23 16:49:20 +0530

    Removed API call from dashboard.js

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,36 +1,16 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import React, {useState,useEffect} from "react";
+import React from "react";
 import { connect } from "react-redux";
 import Highlights from "./Highlights";
 import Profile from "./UserProf";
-import withSpinner from "./spinner/spinner";
 
-import { setCurrentCity } from "../redux/city/city.actions";
-const API_KEY = "5f38a955153345aebbc145451200410";
-const API_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=mumbai&aqi=yes`;
 
-const HighlightsWithSpinner = withSpinner(Highlights);
-const ProfileWithSpinner = withSpinner(Profile);
-function Dashboard(props) {
-  const [isLoading, setIsLoading] = useState(true)
-  useEffect(() => {
-    fetch(API_URL)
-      .then((res) => res.json()) 
-      .then((data) => {
-        props.setCurrentCity({
-          ...data
-        });
-        setIsLoading(false);
-      });
-  },[]);
-  return (
-    <React.Fragment>
-      <HighlightsWithSpinner isLoading={isLoading} />
-      <ProfileWithSpinner isLoading={isLoading} />
-    </React.Fragment>
-  );
+function Dashboard(){
+return <React.Fragment>
+       <Highlights />
+       <Profile />
+     </React.Fragment>
 }
-const mapDispatchToProps = (dispatch) => ({
-  setCurrentCity: (city) => dispatch(setCurrentCity(city)),
+const mapStateToProps = (state) => ({
+  currentCity: state.city.currentCity,
 });
-export default connect(null, mapDispatchToProps)(Dashboard);
+export default connect(mapStateToProps)(Dashboard);
