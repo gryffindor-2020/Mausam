23271349c9f2ff46b710cac72790a444c8534e59
himankgoel <himankgoel12@gmail.com>
2021-05-25 13:21:58 +0530

    Added spinner and debounce in search bar(optimisation)

--- src/Components/AddLocation.js
+++ src/Components/AddLocation.js
@@ -2,26 +2,36 @@ import React, { useState, useEffect } from "react";
 import SearchResults from "./SearchResults";
 import store from "./../redux/store";
 import axios from "axios";
+import withSpinner from "./spinner/spinner";
 
+const SearchResultsWithSpinner = withSpinner(SearchResults);
 function AddLocation() {
   const API_KEY = "5f38a955153345aebbc145451200410";
 
   const [query, setQuery] = useState("");
+  const [isTyping, setIsTyping] = useState(false);
   const [results, setResults] = useState([]);
   function handleChange(e) {
     setQuery(e.target.value);
+    setIsTyping(true);
   }
   function handleClick() {
     setQuery("");
   }
   useEffect(() => {
     const API_URL = `https://api.weatherapi.com/v1/search.json?key=${API_KEY}&q=${query}`;
-    if (query.length >= 3) {
-      axios.get(API_URL).then((res) => setResults(res.data.slice(0, 4)));
-    }
+
+    const delayDebounce = setTimeout(() => {
+      if (query.length >= 3) {
+        axios.get(API_URL).then((res) => setResults(res.data.slice(0, 4)));
+      }
+      setIsTyping(false);
+    }, 500);
+
     store.subscribe(() => {
       handleClick();
     });
+    return () => clearTimeout(delayDebounce);
   }, [query]);
   return (
     <div className="add-location">
@@ -41,7 +51,9 @@ function AddLocation() {
             onClick={handleClick}
           />
         )}
-        {query !== "" && <SearchResults results={results} />}
+        {query !== "" && (
+          <SearchResultsWithSpinner results={results} isLoading={isTyping} />
+        )}
       </div>
     </div>
   );
