526fa9a5c27effc338f43d163cb82432bc063383
himankgoel <himankgoel12@gmail.com>
2021-05-25 12:34:30 +0530

    If no city is found, then that show message added

--- src/Components/SearchResults.js
+++ src/Components/SearchResults.js
@@ -1,11 +1,14 @@
 import React from "react";
-import SearchResult from "./SearchResultIndividual";
+import SearchResultIndividual from "./SearchResultIndividual";
 
 function SearchResults({ results }) {
   return (
     <div className="search-result">
+      {results.length === 0 && (
+        <div className="search-result__text">No city found :(</div>
+      )}
       {results.map((result) => {
-        return <SearchResult data={result} key={result.id} />;
+        return <SearchResultIndividual data={result} key={result.id} />;
       })}
     </div>
   );
